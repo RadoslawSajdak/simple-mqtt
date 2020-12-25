b8b9a63f6f22ae502218f105d6acb87c6f09d436
Radosław Sajdak <sajdak.radoslaw@gmail.com>
2020-12-25 16:06:37 +0100

    Multithreading version. Publishing and subscribing with one topic

--- MQTT_Client.py
+++ MQTT_Client.py
@@ -3,22 +3,27 @@ import socket
 import netifaces as ni
 ni.ifaddresses('wlan0')
 from time import sleep
+import threading
 
 HOST = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
 PORT = 3000
-room = "127.0.0.1"
-payload = "+/cars"
-MESSAGE = b"Hej"
+room = "192.168.0.144"
+
+def reading_thread(socket):
+    while(1):
+        data = socket.recv(1024)
+        print(data)
+
+
 def connect():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((room, PORT))
-        sleep(3)
-        s.sendall(MESSAGE)
-        
+        thrd = threading.Thread(target=reading_thread, args = (s,))
+        thrd.start()
         while(1):
             
-            data = s.recv(1024)
-            print(data)
+            #data = s.recv(1024)
+            #print(data)
             s.sendall(input().encode())
             
         s.shutdown(socket.SHUT_RDWR)
